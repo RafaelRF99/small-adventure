458a1117bdfc07b5bf74f0f0e3e0f29e5dc63ea8
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-08-15 16:45:08 -0300

    feat: clear code

---
+++ src/components/RandomItem/renderDrop.tsx
@@ -0,0 +1,77 @@
+import styles from './RandomItem.module.scss';
+// JSON
+import { acessorios, saque, baus, armadilhas, colares, clava1, clava2, clava3, monstros, pocoes } from './drop';
+
+export function renderDrop (tier: number) {
+  if (tier === 1) {
+    const qtd = acessorios.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = acessorios[random]
+    if (escolha.dano) {
+      return (
+        <span className={styles.render}><p> {escolha.lot + ' +' + escolha.dano} </p> <p> {escolha.effect} </p></span>
+      )
+    }
+    return escolha.lot + '*'
+  }
+  if (tier === 2) {
+    const qtd = saque.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = saque[random]
+    return escolha.lot
+  }
+  if (tier === 3) {
+    const qtd = baus.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = baus[random]
+    return escolha.lot
+  }
+  if (tier === 4) {
+    const qtd = armadilhas.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = armadilhas[random]
+    return (
+      <span className={styles.render}><p> {escolha.trap} </p> <p> {escolha.effect} </p></span>
+    )
+  }
+  if (tier === 5) {
+    const qtd = colares.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = colares[random]
+    return (
+      <span className={styles.render}><p> {escolha.lot} </p> <p> {escolha.effect} </p></span>
+    )
+  }
+  if (tier === 6) {
+    const qtd = clava1.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = clava1[random]
+    return escolha.bonus
+  }
+  if (tier === 7) {
+    const qtd = clava2.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = clava2[random]
+    return escolha.bonus
+  }
+  if (tier === 8) {
+    const qtd = clava3.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = clava3[random]
+    return escolha.bonus
+  }
+  if (tier === 9) {
+    const qtd = monstros.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = monstros[random]
+    return escolha.mob
+  }
+  if (tier === 10) {
+    const qtd = pocoes.length
+    const random = Math.floor(Math.random() * qtd)
+    const escolha = pocoes[random]
+    return escolha.mob
+  }else {
+    return 'Erro'
+  }
+}
