0dac4ea4df57032e3f3cd0f2db485c8a823f4d5b
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-08-16 17:01:37 -0300

    style: title

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,13 +1,15 @@
-import ButtonFloat from '@/components/ButtonFloat';
 import styles from './page.module.scss'
+// FONTS
+import { title } from '../../fonts/fonts'
 // COMPONENTS
+import ButtonFloat from '@/components/ButtonFloat';
 import RandomMaps from "@/components/RandomMaps";
 
 export default function Maps() {
 
     return (
         <div className={styles.container}>
-            <h1>Maps</h1>
+            <h1 className={`${styles.title} ${title.className}`}>Maps</h1>
             <div className={styles.cartas}>
                 <RandomMaps carta='A' />
                 <RandomMaps carta='2' />
